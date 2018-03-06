7eb7b927ac84fc73ca3b9b1025de1d6cf2ec46b5
Karol Kozakowski <cosaquee@gmail.com>
2018-03-06 21:30:09 +0100

    feature: Check if token is correct

--- project.clj
+++ project.clj
@@ -4,11 +4,17 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
+                 [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.9.7"]
+            [cider/cider-nrepl "0.17.0-SNAPSHOT"]
+            [refactor-nrepl "2.4.0-SNAPSHOT"]]
   :ring {:handler booker-books.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [clj-http "3.7.0"]
                         [ring-cors "0.1.11"]
+                        [org.clojure/core.match "0.3.0-alpha5"]
+                        [org.clojure/data.json "0.2.6"]
                         [ring/ring-mock "0.3.0"]]}})
+
--- src/booker_books/handler.clj
+++ src/booker_books/handler.clj
@@ -2,23 +2,30 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [clj-http.client :as http]
+            [clojure.string :as str]
+            [clojure.core.match :refer [match]]
+            [clojure.data.json :as json]
             [ring.middleware.cors :refer [wrap-cors]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/authors" [] "Hello World")
   (route/not-found "Not Found"))
 
+;; TODO: Error handling
 (defn check-auth [handler]
   (fn [request]
-    (println "It works")
-    (println request)
-    (handler request)))
+    (let [token-header (get-in request [:headers "authorization"])]
+      (let [token (last (str/split token-header #" "))]
+        (let [response (http/post "http://localhost:4000/auth" {:content-type :json :body (json/write-str {:token token})})]
+          (match [(get (json/read-str (get response :body)) "success")]
+                 [true] (handler request)
+                 [false] (println "Something went wrong")))
+      ))))
 
 (def app
   (-> app-routes
-    (wrap-defaults api-defaults)
-    (check-auth)
-    (wrap-cors :access-control-allow-origin [#"http://localhost:8080"]
-               :access-control-allow-methods [:get :put :post :delete :options]))
-  )
+      (check-auth)
+      (wrap-cors :access-control-allow-origin [#"http://localhost:8080"]
+                 :access-control-allow-methods [:get :put :post :delete :OPTIONS])
+      (wrap-defaults api-defaults)))
